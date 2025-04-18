44de9bedff70417391c224f521ff1255718944f7
bhava008 <bhava008@umn.edu>
2025-04-17 23:49:49 -0500

    testing stuff

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // Typing effect
-const text = "Empowering people through code & creativity.";
+const text = "Aspiring Software Engineer | Mobile Dev | UX Enthusiast";
 let index = 0;
-function type() {
+const typingText = document.getElementById("typing-text");
+
+function typeEffect() {
   if (index < text.length) {
-    document.getElementById("typing-text").textContent += text.charAt(index);
+    typingText.innerHTML += text.charAt(index);
     index++;
-    setTimeout(type, 50);
+    setTimeout(typeEffect, 70);
   }
 }
-window.onload = type;
+window.onload = typeEffect;
 
-// Dark mode toggle
+// Theme toggle
 document.getElementById("toggle-theme").addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
